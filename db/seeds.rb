876026ab56011d44683473426bb12f988116f3be
Sid Mann <sjmann2@outlook.com>
2022-09-05 20:33:56 -0500

    Remove duplicate pet

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,6 @@ shelter_1 = Shelter.find_or_create_by!(name: 'Aurora shelter', city: 'Aurora, CO
 shelter_2 = Shelter.find_or_create_by!(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
 shelter_3 = Shelter.find_or_create_by!(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
 
-pet_3 = shelter_3.pets.find_or_create_by!(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
-pet_4 = shelter_1.pets.find_or_create_by!(name: 'Ann', breed: 'ragdoll', age: 5, adoptable: true)
 
 pet_1 = shelter_1.pets.find_or_create_by!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald')
 pet_2 = shelter_1.pets.find_or_create_by!(adoptable: true, age: 7, breed: 'yorkie', name: 'Mr. Pig')
@@ -21,6 +19,7 @@ pet_5 = shelter_3.pets.find_or_create_by!(adoptable: true, age: 6, breed: 'Huske
 pet_6 = shelter_3.pets.find_or_create_by!(adoptable: true, age: 2, breed: 'Calico', name: 'Potato Chip')
 pet_7 = shelter_3.pets.find_or_create_by!(adoptable: false, age: 5, breed: 'tuxedo shorthair', name: 'Mr. Pirate')
 pet_8 = shelter_1.pets.find_or_create_by!(adoptable: true, age: 3, breed: 'shorthair', name: 'Clawdia')
+pet_9 = shelter_1.pets.find_or_create_by!(adoptable: true, age: 5, breed: 'ragdoll', name: 'Ann')
 
 application_1 = Application.find_or_create_by!(name: "Shelby Waters", street_address: "274 West 11th St", city: "Myers Flatt", state: "NJ", zipcode: 12447, status: "Pending", description: "I'm Lonely")
 application_2 = Application.find_or_create_by!(name: "Florence Bigsby", street_address: "202 E Washington Ave", city: "Madison", state: "WI", zipcode: 60637, status: "Pending", description: "I love cats")
